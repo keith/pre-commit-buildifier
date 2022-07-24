9716f1ea868700fe9e5ed21191cae87b083cf333
Keith Smiley <keithbsmiley@gmail.com>
2022-07-24 11:06:04 -0700

    Add getshas.sh
    Useful for updates

---
+++ getshas.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+readonly version="$1"
+echo "readonly version=$version"
+
+for os in darwin linux
+do
+  for arch in amd64 arm64
+  do
+    filename=buildifier-$os-$arch
+    url=https://github.com/bazelbuild/buildtools/releases/download/$version/$filename
+    bin=$(mktemp)
+    curl --fail --silent -L "$url" -o "$bin"
+    sha=$(sha256sum "$bin" | cut -d ' ' -f 1)
+    echo "# shellcheck disable=SC2034"
+    echo "readonly ${os}_${arch}_sha=$sha"
+  done
+done
