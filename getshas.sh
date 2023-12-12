7288b912173dad8c99ae9b07433d565402756971
Keith Smiley <keithbsmiley@gmail.com>
2023-12-12 08:52:00 -0800

    Discard changes to getshas.sh

--- getshas.sh
+++ getshas.sh
@@ -13,9 +13,6 @@ do
     url=https://github.com/bazelbuild/buildtools/releases/download/$version/$filename
     bin=$(mktemp)
     curl --fail --silent -L "$url" -o "$bin"
-    if [[ "$os" == darwin ]] && ! type sha256sum > /dev/null; then
-       alias sha256sum="shasum -a 256"
-    fi
     sha=$(sha256sum "$bin" | cut -d ' ' -f 1)
     echo "# shellcheck disable=SC2034"
     echo "readonly ${os}_${arch}_sha=$sha"
