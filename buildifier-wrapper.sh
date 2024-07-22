2466581c531f404b7687911b109e25357d45bc9d
pat-jpnk <jpnksoftware@protonmail.com>
2024-07-22 23:12:25 +0200

    initial commit - support for windows-amd64.exe

--- buildifier-wrapper.sh
+++ buildifier-wrapper.sh
@@ -3,19 +3,17 @@
 set -euo pipefail
 
 readonly version=v6.4.0
-# shellcheck disable=SC2034
 readonly darwin_amd64_sha=eeb47b2de27f60efe549348b183fac24eae80f1479e8b06cac0799c486df5bed
-# shellcheck disable=SC2034
 readonly darwin_arm64_sha=fa07ba0d20165917ca4cc7609f9b19a8a4392898148b7babdf6bb2a7dd963f05
-# shellcheck disable=SC2034
 readonly linux_amd64_sha=be63db12899f48600bad94051123b1fd7b5251e7661b9168582ce52396132e92
-# shellcheck disable=SC2034
 readonly linux_arm64_sha=18540fc10f86190f87485eb86963e603e41fa022f88a2d1b0cf52ff252b5e1dd
-
+readonly windows_amd64_sha=9f7d24d9b4f4a2eb0ae4caffef02e8c3de8708a497f1d2e9a6de79e25a1d9f2e
 
 os=linux
 if [[ $OSTYPE == darwin* ]]; then
   os=darwin
+elif [[ $OSTYPE == msys* || $OSTYPE == cygwin* || $OSTYPE == win32 ]]; then
+  os=windows
 fi
 
 arch=amd64
@@ -23,7 +21,12 @@ if [[ $(uname -m) == arm64 ]] || [[ $(uname -m) == aarch64 ]]; then
   arch=arm64
 fi
 
-readonly filename=buildifier-$os-$arch
+if [[ $os == "windows" ]]; then
+  readonly filename=buildifier-windows-amd64.exe
+else
+  readonly filename=buildifier-$os-$arch
+fi
+
 readonly url=https://github.com/bazelbuild/buildtools/releases/download/$version/$filename
 readonly binary_dir=~/.cache/pre-commit/buildifier/$os-$arch-$version/buildifier
 readonly binary=$binary_dir/buildifier-$1
@@ -35,6 +38,7 @@ fi
 
 readonly sha_accessor=${os}_${arch}_sha
 readonly sha="${!sha_accessor}"
+
 mkdir -p "$binary_dir"
 tmp_binary=$(mktemp)
 
@@ -50,12 +54,9 @@ fi
 
 if echo "$sha  $tmp_binary" | $shabin --check --status; then
   chmod +x "$tmp_binary"
-
-  # Protect against races of concurrent hooks downloading the same binary
   if [[ ! -x "$binary" ]]; then
     mv "$tmp_binary" "$binary"
   fi
-
   exec "$binary" "$@"
 else
   echo "error: buildifier sha mismatch" >&2
